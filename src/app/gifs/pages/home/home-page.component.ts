7f257c961a89ffe0a25f686678145f1af0750c8d
Celia <celia2795@gmail.com>
2023-05-25 09:57:15 +0200

    lazy loading of images

--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { GifsService } from '../../services/gifs.service';
 import { Gif } from '../../interfaces/gifs.interfaces';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-home-page',
